06cfe5efe20c98bfeff1ae6e9b29c7dd3d43d4b5
Stephen Rubio <stephenrubio94@gmail.com>
2014-11-15 19:07:42 -0500

    MainActivity works, links to final activity

--- pandorakey4/src/fordhamcss/pandorakey4/MainActivity.java
+++ pandorakey4/src/fordhamcss/pandorakey4/MainActivity.java
@@ -2,17 +2,54 @@
 package fordhamcss.pandorakey4;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
+	private Button start_button;
+	private Button stop_button;
+	private Button report_button;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        start_button 	= (Button)findViewById(R.id.start_button);
+        stop_button 	= (Button)findViewById(R.id.stop_button);
+        report_button 	= (Button)findViewById(R.id.report_button);
+        
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Start the service
+            	System.out.println("Starting service");
+            }
+        });
+        
+        stop_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Stop the service
+            	System.out.println("Stopping service");
+            }
+        });
+        
+        report_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	System.out.println("Opening reports");
+                Intent i = new Intent(MainActivity.this, FinalActivity.class); 
+                startActivityForResult(i, 0);
+            }
+        });
+        
     }
 
 
